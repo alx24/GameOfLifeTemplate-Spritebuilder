a8df9eb9c3186b806c5e7ea0e7a71c5254255b7b
alex ferreira <abrex18@hotmail.com>
2015-02-23 11:39:28 -0300

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -13,15 +13,15 @@
 
 // estas sao variaveis  que nao podem ser alterada.
 
-static const int GRID_ROWNS = 10;
-static const int GRID_COLUMNS = 8;
+static const int GRID_ROWS = 8;
+static const int GRID_COLUMNS = 10;
 
 @implementation Grid{
     
     // criando colunas e celulas.
     NSMutableArray *_gridArray;
-    float _cellWidht;
-    float _cellHeight;
+    float _celWidth;
+    float _celHeight;
     
     
 }
@@ -37,7 +37,45 @@ static const int GRID_COLUMNS = 8;
     
 }
 
--(void)setupGrid{
+
+- (void)setupGrid
+{
+    // divide the grid's size by the number of columns/rows to figure out the right width and height of each cell
+    _celWidth = self.contentSize.width / GRID_COLUMNS;
+    _celHeight = self.contentSize.height / GRID_ROWS;
+    
+    float x = 0;
+    float y = 0;
+    
+    // initialize the array as a blank NSMutableArray
+    _gridArray = [NSMutableArray array];
+    
+    // initialize Creatures
+    for (int i = 0; i < GRID_ROWS; i++) {
+        // this is how you create two dimensional arrays in Objective-C. You put arrays into arrays.
+        _gridArray[i] = [NSMutableArray array];
+        x = 0;
+        
+        for (int j = 0; j < GRID_COLUMNS; j++) {
+            Creature *creature = [[Creature alloc] initCreature];
+            creature.anchorPoint = ccp(0, 0);
+            creature.position = ccp(x, y);
+            [self addChild:creature];
+            
+            // this is shorthand to access an array inside an array
+            _gridArray[i][j] = creature;
+            
+            // make creatures visible to test this method, remove this once we know we have filled the grid properly
+            creature.isAlive = YES;
+            
+            x+=_celWidth;
+        }
+        
+        y += _celHeight;
+    }
+}
+
+/*-(void)setupGrid{
     
     // Dividir o tamanho da rede pelo número de colunas / linhas para descobrir a largura direita e altura de cada célula
     
@@ -83,6 +121,7 @@ static const int GRID_COLUMNS = 8;
    }
 
 }
+ */
 
 @end
 
